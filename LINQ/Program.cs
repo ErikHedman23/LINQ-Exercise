79c4f6daec594b8ccd4f087ebadf4a50349b0bdf
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-08-08 12:15:04 -0700

    Added a console.writeline

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,6 +6,7 @@
         {
             var games = new List<string>() { "Mario Cart", "Jenga", "Pac-Man"};
             var gameNames = games.OrderByDescending(x => x.Length).ToList();
+            Console.WriteLine("Here is a list of game names in descending order by length of each name:\n");
             foreach (var gameName in gameNames)
             {
                 Console.WriteLine(gameName);
