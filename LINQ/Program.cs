575f5620db489b76352c921e0910cccb8cbb6fed
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-08-09 08:38:36 -0700

    Changed the syntax of the list

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            var games = new List<string>() { "Mario Cart", "Jenga", "Pac-Man"};
+            var games = new List<string>() 
+            { "Mario Cart", 
+              "Jenga", 
+              "Pac-Man"};
             var gameNames = games.OrderByDescending(x => x.Length).ToList();
             Console.WriteLine("Here is a list of game names in descending order by length of each name:\n");
             foreach (var gameName in gameNames)
